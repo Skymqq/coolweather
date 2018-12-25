fca2045b2c6a7c263f1a18743ce42e541eb21d00
Skymqq <1538974565@qq.com>
2018-12-25 15:51:32 +0800

    完成遍历省市县三级列表的功能。

---
+++ app/src/main/java/com/example/administrator/coolweather/util/HttpUtil.java
@@ -0,0 +1,22 @@
+package com.example.administrator.coolweather.util;
+
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+
+public class HttpUtil {
+
+    /**
+     *
+     * @param address   请求地址
+     * @param callback  注册一个回调来处理服务器响应
+     */
+
+    public static void sendOkHttpRequest(String address,okhttp3.Callback callback){
+        OkHttpClient client=new OkHttpClient();
+        Request request=new Request.Builder()
+                .url(address)
+                .build();
+        client.newCall(request).enqueue(callback);
+    }
+
+}
